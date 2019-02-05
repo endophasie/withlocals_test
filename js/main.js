e98d2237b0e5b4ac9c983cebf951be0a559d3dda
endophasie <albina.tokarchuk@gmail.com>
2019-02-05 17:52:16 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const SERVER_URL = 'https://www.withlocals.com/api/v1/experience/searchWithOthers?adults=2&children=0&date.from=2019-05-10&date.to=2019-05-10&fieldset=reactsearch&lang=en&location.city=rome&location.country=italy&page=1&pagesize=47';
+const CORS_URL = 'https://crossorigin.me/';
 
 const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
@@ -14,7 +15,7 @@ const toJSON = (response) => response.json();
 
 class Loader {
   static loadData() {
-    return fetch(`${SERVER_URL}`)
+    return fetch(`${CORS_URL}${SERVER_URL}`)
       .then(checkStatus)
       .then(toJSON);
   }
